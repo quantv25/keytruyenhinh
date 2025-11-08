d63d9098c218130178247182b93f7bc775760947
quantv25 <Trinhquanhpv@gmail.com>
2025-11-08 14:01:19 +0700

    Delete pages/api/health.js

--- pages/api/health.js
+++
@@ -1,9 +0,0 @@
-export default (req,res)=>res.status(200).json({
-  ok:true,
-  env:{
-    GITHUB_REPO:process.env.GITHUB_REPO,
-    GITHUB_BRANCH:process.env.GITHUB_BRANCH,
-    CODES_PATH:process.env.CODES_PATH,
-    HAS_TOKEN:!!process.env.GITHUB_TOKEN
-  }
-});
